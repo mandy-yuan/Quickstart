6c659af2449bd3cee726d784cb61d484d29bb1db
mandy <mandy.yuan19@gmail.com>
2026-01-21 13:44:29 -0800

    shooter speed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShooterSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShooterSubsystem.java
@@ -5,7 +5,8 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 
 public class ShooterSubsystem {
-    private final DcMotorEx shooterMotor;
+    private final DcMotorEx shooterMotor1;
+    private final DcMotorEx shooterMotor2;
     private final double MotorTicksPerRev = 28;
     private final double MotorGearRatio = (double) 1/19.2;
 
@@ -15,8 +16,9 @@ public class ShooterSubsystem {
             0,
             0
     );
-    public ShooterSubsystem(DcMotorEx shooterMotor) {
-        this.shooterMotor = shooterMotor;
+    public ShooterSubsystem(DcMotorEx shooterMotor, DcMotorEx shooterMotor1, DcMotorEx shooterMotor2) {
+        this.shooterMotor1 = shooterMotor1;
+        this.shooterMotor2 = shooterMotor2;
         shooterMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         shooterMotor.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidfCoefficients);
         shooterMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -25,10 +27,12 @@ public class ShooterSubsystem {
     }
 
     public void revToRPM(double RPM) {
-        this.shooterMotor.setVelocity(rpmToTicksPerSecond(RPM));
+        this.shooterMotor2.setVelocity(rpmToTicksPerSecond(RPM));
+        this.shooterMotor1.setVelocity(rpmToTicksPerSecond(RPM));
     }
     public void stop() {
-        this.shooterMotor.setVelocity(0);
+        this.shooterMotor1.setVelocity(0);
+        this.shooterMotor2.setVelocity(0);
         //try set voltage
     }
 
